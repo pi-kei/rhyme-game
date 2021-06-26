d48f90e7ac3e0047dad1f4f8aa251681fb6036a4
pavel <kim110488@gmail.com>
2021-06-26 08:06:32 +0300

    Add ability to go back when picking avatar

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -386,6 +386,8 @@ function Login({
     const [customId, setCustomId] = useState<string>(storage.getItem('uuid') || '');
     const [avatar, setAvatar] = useState<string>(storage.getItem('avatar') || '');
     const { isMuted, toggleMuted } = useSoundsHelper(soundsHelper);
+    const [avatarHistory, setAvatarHistory] = useState<string[]>(avatar ? [avatar] : []);
+    const [avatarHistoryIndex, setAvatarHistoryIndex] = useState<number>(avatarHistory.length ? 0 : -1);
 
     const randomCustomId = () => {
         const newCustomId = nanoid();
@@ -399,11 +401,12 @@ function Login({
         setDefaultUserName(newUserName);
     };
 
-    const randomAvatar = () => {
-        //const newAvatar = `data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(multiavatar(nanoid()))))}`;
-        const newAvatar = process.env.NODE_ENV === 'production' && process.env.REACT_APP_GET_AVATARS_FROM === 'sameOrigin' ? `${process.env.PUBLIC_URL}/avatar/${nanoid()}` : `https://api.multiavatar.com/${nanoid()}.svg`;
-        storage.setItem('avatar', newAvatar);
-        setAvatar(newAvatar);
+    const nextAvatar = () => {
+        setAvatarHistoryIndex(prevAvatarHistoryIndex => prevAvatarHistoryIndex + 1);
+    };
+
+    const prevAvatar = () => {
+        setAvatarHistoryIndex(prevAvatarHistoryIndex => Math.max(0, prevAvatarHistoryIndex - 1));
     };
 
     const onUserNameChange = (event: React.ChangeEvent, data: InputOnChangeData) => {
@@ -422,9 +425,27 @@ function Login({
         }
 
         if (!avatar) {
-            randomAvatar();
+            nextAvatar();
         }
     }, []);
+
+    useEffect(() => {
+        if (avatarHistoryIndex >= avatarHistory.length) {
+            //const newAvatar = `data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(multiavatar(nanoid()))))}`;
+            const newAvatar = process.env.NODE_ENV === 'production' && process.env.REACT_APP_GET_AVATARS_FROM === 'sameOrigin' ? `${process.env.PUBLIC_URL}/avatar/${nanoid()}` : `https://api.multiavatar.com/${nanoid()}.svg`;
+            const index = avatarHistoryIndex;
+            setAvatarHistory(prevAvatarHistory => {
+                const newAvatarHistory = prevAvatarHistory.concat();
+                newAvatarHistory[index] = newAvatar;
+                return newAvatarHistory;
+            });
+        }
+        if (avatarHistory[avatarHistoryIndex]) {
+            const newAvatar = avatarHistory[avatarHistoryIndex];
+            storage.setItem('avatar', newAvatar);
+            setAvatar(newAvatar);
+        }
+    }, [avatarHistoryIndex, avatarHistory]);
 
     if (!ready) {
         return null;
@@ -448,12 +469,25 @@ function Login({
                 <Grid.Row textAlign="center">
                     <Grid.Column>
                         <Button
+                            icon="angle left"
                             as='a'
                             compact
-                            onClick={randomAvatar}
+                            onClick={prevAvatar}
+                            disabled={avatarHistoryIndex <= 0}
+                        />
+                        <Button
+                            as='a'
+                            compact
+                            onClick={nextAvatar}
                         >
                             <Image src={avatar} size='tiny' />
                         </Button>
+                        <Button
+                            icon="angle right"
+                            as='a'
+                            compact
+                            onClick={nextAvatar}
+                        />
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row textAlign="center">
